a66fd66a8b0c9d39dfb99b7a557b231410a27228
Salt4f <47352634+Salt4f@users.noreply.github.com>
2019-05-01 12:50:29 +0200

    Update

--- Documentation/html/search/functions_2.js
+++ Documentation/html/search/functions_2.js
@@ -3,7 +3,7 @@ var searchData=
   ['elementos',['elementos',['../class_freq_table.html#afb857aca3fae2f37b14a93407cdf1551',1,'FreqTable']]],
   ['es_5fdel_5fidioma',['es_del_idioma',['../class_idioma.html#a4668bf02f948cc873e7342147172145c',1,'Idioma']]],
   ['escribir_5farbol',['escribir_arbol',['../class_codigo.html#ae715e2ad01a25064f484ede923d0fb75',1,'Codigo']]],
-  ['escribir_5fcodigos',['escribir_codigos',['../class_codigo.html#aff1dd5ba7f1ad429d14cae824a464fbb',1,'Codigo::escribir_codigos()'],['../class_diccionario.html#a4abe1658a4945c22c923ccb299b32054',1,'Diccionario::escribir_codigos()'],['../class_idioma.html#ad33ab5a7b9a1567057f9916bf868277e',1,'Idioma::escribir_codigos()']]],
+  ['escribir_5fcodigos',['escribir_codigos',['../class_codigo.html#a2b5d3a02fad8dcecfa0c4ee55e02a989',1,'Codigo::escribir_codigos()'],['../class_diccionario.html#a4abe1658a4945c22c923ccb299b32054',1,'Diccionario::escribir_codigos()'],['../class_idioma.html#a768902d7b18678f04d89d18b7336eb96',1,'Idioma::escribir_codigos()']]],
   ['escribir_5ftabla',['escribir_tabla',['../class_freq_table.html#a1739e3f11f08f894e6fe00e48041d263',1,'FreqTable::escribir_tabla()'],['../class_idioma.html#a26effb654be9fa4a6ccc5fc771f4efc0',1,'Idioma::escribir_tabla()']]],
   ['escribir_5ftabla_5ffrecuencias',['escribir_tabla_frecuencias',['../class_diccionario.html#a4cb2e9403b93acb282f412294c40d20e',1,'Diccionario']]],
   ['escribir_5ftreecode',['escribir_treecode',['../class_diccionario.html#acf3146430ce548e1840b888cfa59aa1f',1,'Diccionario::escribir_treecode()'],['../class_idioma.html#ac309e4d7060759099aa5ce3ed7150435',1,'Idioma::escribir_treecode()']]],
--- Documentation/html/search/functions_4.js
+++ Documentation/html/search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['idioma',['Idioma',['../class_idioma.html#a6722a621ce03825772493e67e5a17215',1,'Idioma']]],
-  ['insercion',['insercion',['../_codigo_8cc.html#a24f272155c58c71a11d73d72d591513c',1,'Codigo.cc']]]
+  ['insercion',['insercion',['../_codigo_8cc.html#a1d46ed9a92f1c1dc9344cc04fe7b3a9e',1,'Codigo.cc']]]
 ];
